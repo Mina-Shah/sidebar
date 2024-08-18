fca6bef7ecf4fdd5c899871ed00357433b95bd65
Mina-Shah <shahmina814@gmail.com>
2024-08-18 21:04:31 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const toggleBtn = document.querySelector('.sidebar-toggle')
+const closeBtn = document.querySelector('.close-btn')
+const sidebar = document.querySelector('.sidebar')
+
+toggleBtn.addEventListener('click', function () {
+    //  if (links.classList.contains('show-links')) {
+    //     links.classList.remove('show-links')
+    // } else {
+    //     links.classList.add('show-links')
+    // } 
+    sidebar.classList.toggle('show-sidebar');
+})
